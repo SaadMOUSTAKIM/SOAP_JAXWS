22e8136d727fc341ad528806828b70a7c60f2deb
Saad MOUSTAKIM <saadmoustakim2@gmail.com>
2022-10-30 18:47:37 +0100

    jaxws

--- src/main/java/ws/BanqueService.java
+++ src/main/java/ws/BanqueService.java
@@ -1,16 +1,22 @@
 package ws;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
 import java.util.Date;
 import java.util.List;
 
-
+@WebService(serviceName = "BanqueWS")
 public class BanqueService {
-    public double conversion(double mt ){
+    @WebMethod(operationName = "Convert")
+    public double conversion(@WebParam(name = "montant") double mt ){
         return mt*10.54;
     }
-    public Compte getCompte(int code){
+    @WebMethod
+    public Compte getCompte(@WebParam(name = "code") int code){
         return new Compte(code, Math.random()*9888,new Date());
     }
+    @WebMethod
     public List<Compte> listComptes(){
         return List.of(
                 new Compte(1, Math.random()*9888,new Date()),
